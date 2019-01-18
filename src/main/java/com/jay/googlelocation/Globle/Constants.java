15afab7731811b9996a2a934983c3b66046253d0
jaykumar <jaykumar@peerbits.com>
2019-01-18 14:43:06 +0530

    First Commit

---
+++ src/main/java/com/jay/googlelocation/Globle/Constants.java
@@ -0,0 +1,17 @@
+package com.jay.googlelocation.Globle;
+
+/**
+ * Created by lg on 2/15/2017.
+ */
+
+public class Constants {
+
+    public static final String TYPE= "type";
+    public static final int LOCATION_FETCH=0;
+    public static final int LOCATION_PICK=1;
+
+    public static final String LATITUDE="Latitude";
+    public static final String LONGITUDE="Longitude";
+
+    public static final String LOCATION_BROADCAST_KEY="com.jay.googlelocation.SEND_UPDATES";
+}
